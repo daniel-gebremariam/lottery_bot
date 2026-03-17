c2fb71ca6bebfb42451dae5eb4792035daae50b5
daniel-gebremariam <daneximpex@gmail.com>
2026-03-17 19:00:57 +0300

    Add configuration settings for Telegram bot

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = "TELEGRAM_BOT_TOKEN"
+
+ADMIN_ID = 123456789  # your telegram id
+
+DATABASE_URL = "postgresql://user:password@localhost/lotterydb"
+
+TICKET_PRICE = 2000
+MAX_TICKETS = 1000
