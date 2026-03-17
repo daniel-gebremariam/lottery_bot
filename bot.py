07e962bc20018e382d90f5e273ce3efa79b77507
daniel-gebremariam <daneximpex@gmail.com>
2026-03-17 18:59:16 +0300

    Implement basic Telegram bot structure

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+
+from config import BOT_TOKEN
+from handlers import start, join_lottery, handle_message, handle_photo
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(join_lottery, pattern="join"))
+
+app.add_handler(MessageHandler(filters.TEXT, handle_message))
+app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
+
+app.run_polling()
